e9e9365d05492ff95bc5897311c295a6335cff63
Michael <opcache@gmail.com>
2023-08-10 04:31:30 +0300

    finally have smth

---
+++ MessageEventArgs.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+
+namespace MANIFESTA
+{
+    internal class MessageProcessor
+    {
+        private readonly ITelegramBotClient _botClient;
+        private readonly Message _message;
+
+        public MessageProcessor(ITelegramBotClient botClient, Message message)
+        {
+            _botClient = botClient;
+            _message = message;
+        }
+
+        public async Task ProcessMessage()
+        {
+            string responseMessage = "";
+
+            if (_message.Text.Contains("services"))
+            {
+                responseMessage = "We provide legal and accounting services.";
+            }
+            // Додайте інші обробники для різних команд тут
+
+            else
+            {
+                responseMessage = "Hello! How can I assist you? You can ask about our services or departments.";
+            }
+
+            await _botClient.SendTextMessageAsync(_message.Chat.Id, responseMessage);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+var botClient = new TelegramBotClient("6558637896:AAFn-y5PLNvv_BfQhJEyhEWOPGItg3GCBX4");
+
+using CancellationTokenSource cts = new();
+
+// StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+ReceiverOptions receiverOptions = new()
+{
+    AllowedUpdates = Array.Empty<UpdateType>() // receive all update types except ChatMember related updates
+};
+
+botClient.StartReceiving(
+    updateHandler: HandleUpdateAsync,
+    pollingErrorHandler: HandlePollingErrorAsync,
+    receiverOptions: receiverOptions,
+    cancellationToken: cts.Token
+);
+
+var me = await botClient.GetMeAsync();
+
+Console.WriteLine($"Start listening for @{me.Username}");
+Console.ReadLine();
+
+// Send cancellation request to stop bot
+cts.Cancel();
+
+
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+    // Only process Message updates: https://core.telegram.org/bots/api#message
+    if (update.Message is not { } message)
+        return;
+    // Only process text messages
+    if (message.Text is not { } messageText)
+        return;
+
+    var chatId = message.Chat.Id;
+
+    Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
+    ReplyKeyboardMarkup replyKeyboardMarkup = new(new[]
+{
+    new KeyboardButton[] { "Наші послуги", "Залишити заявку ☎️" },
+})
+    {
+        ResizeKeyboard = true
+    };
+
+    Message sentMessage = await botClient.SendTextMessageAsync(
+        chatId: chatId,
+        text: "Choose a response",
+        replyMarkup: replyKeyboardMarkup,
+        cancellationToken: cancellationToken);
+    // Echo received message text
+    Message Sentmessage = await botClient.SendTextMessageAsync(
+    chatId: chatId,
+    text: "*More info:* ",
+    parseMode: ParseMode.MarkdownV2,
+    disableNotification: true,
+    replyToMessageId: update.Message.MessageId,
+    replyMarkup: new InlineKeyboardMarkup(
+        InlineKeyboardButton.WithUrl(
+            text: "Check Our Instagram",
+            url: "https://www.instagram.com/manifesta_consult/")),
+    cancellationToken: cancellationToken);
+    Console.WriteLine(
+    $"{message.From.FirstName} sent message {message.MessageId} " +
+    $"to chat {message.Chat.Id} at {message.Date.ToLocalTime}. " +
+    $"It is a reply to message {message.ReplyToMessage.MessageId} " +
+    $"and has {message.Entities.Length} message entities.");
+    
+}
+
+Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    var ErrorMessage = exception switch
+    {
+        ApiRequestException apiRequestException
+            => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+        _ => exception.ToString()
+    };
+
+    Console.WriteLine(ErrorMessage);
+    return Task.CompletedTask;
+}
